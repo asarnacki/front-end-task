df756d87f1a6020fc81f34784ad792b42589d888
Artur <sarnackiartur@gmail.com>
2022-03-30 17:01:30 +0200

    small changes

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,9 +3,8 @@ import { FunctionComponent } from "react";
 const Header: FunctionComponent = () => {
   return (
     <section className="hero is-info">
-      <div className="hero-body">
-        <p className="title">Workate</p>
-        <p className="subtitle">Frontend Task</p>
+      <div className="hero-body">     
+        <p className="title">Frontend Task</p>
       </div>
     </section>
   );
